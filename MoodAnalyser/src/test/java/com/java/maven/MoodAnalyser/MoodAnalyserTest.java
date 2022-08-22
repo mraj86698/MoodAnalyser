d58bbc3401ebee4aed8a012854be0392a24823ec
MohanRaj <mraj86698@gmail.com>
2022-08-22 16:42:04 +0530

    Added to Display Message

---
+++ MoodAnalyser/src/test/java/com/java/maven/MoodAnalyser/MoodAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.java.maven.MoodAnalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MoodAnalyserTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
