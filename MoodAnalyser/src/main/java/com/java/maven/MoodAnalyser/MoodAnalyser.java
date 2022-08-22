81cdf4525b9882f421ded4a2ed731f5a16bb7e9f
MohanRaj <mraj86698@gmail.com>
2022-08-22 18:11:53 +0530

    Added to Null or Empty Mood throw Custom Exception

--- MoodAnalyser/src/main/java/com/java/maven/MoodAnalyser/MoodAnalyser.java
+++ MoodAnalyser/src/main/java/com/java/maven/MoodAnalyser/MoodAnalyser.java
@@ -1,5 +1,7 @@
 package com.java.maven.MoodAnalyser;
 
+import com.java.maven.MoodAnalyser.MoodAnalyserException.ExceptionType;
+
 public class MoodAnalyser
 {
 	private String message;
@@ -19,26 +21,37 @@ public class MoodAnalyser
 		this.message = message;
 	}
 
+
 	/**
 	 * Given Message, ability to analyse  and Respond Happy or Sad
 	 * Call Anallyse Mood Function with Message as  parameter and return Happy or Sad Mood
 	 * @return
 	 */
 
-	public String analyzeMessage() {
+	public String analyzeMessage()throws MoodAnalyserException {
 		/**
 		 * Handle Exception if User Provides Invalid Mood
-		 * Given Null Mood Should Return Happy
+		 * In case of NULL or Empty Mood throw Custom Exception MoodAnalysisException
+		 * Use Enum to differentiate the Mood Analysis Errors
 		 */
 		try {
+			/**
+			 * Given Empty Mood should Throw Mood Analysis Exception indicating EmptyMood
+			 */
+			if(message.length()==0)
+				throw new MoodAnalyserException(ExceptionType.ENTERED_EMPTY, "Mood cannot be empty");
 			String msg=message.toLowerCase();
 			if (msg.contains("sad")) {
 				return "SAD";
 			}else {
 				return "HAPPY";
 			}
-		}catch(NullPointerException exception) {
-			return "HAPPY";
+		}
+		/**
+		 * Given Null Mood should throw Mood Analysis Exception
+		 */
+		catch(NullPointerException exception) {
+			throw new MoodAnalyserException(ExceptionType.ENTERED_NULL, "Mood cannot be null");
 		}
 	}
 }
--- MoodAnalyser/src/test/java/com/java/maven/MoodAnalyser/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/java/maven/MoodAnalyser/MoodAnalyserTest.java
@@ -1,33 +1,71 @@
 package com.java.maven.MoodAnalyser;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class MoodAnalyserTest
-{
-private MoodAnalyser analyser;
+public class MoodAnalyserTest {
+
 
 	/**
-	 * Testing for Sad case
-	 * Given “I am in Sad Mood” message Should Return SAD
+	 * Testing for Sad case Given “I am in Sad Mood” message Should Return SAD
 	 * analyseMood method can just return SAD to pass this Test Case (TC)
 	 */
 	@Test
-	public void message_WhenSad_MustReturn_Sad() {
-		analyser = new MoodAnalyser("I am in SAD Mood");
-		Assert.assertEquals("SAD", analyser.analyzeMessage());
+	public void givenMessage_WhenSad_ShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+		String mood = null;
+		try {
+			mood = moodAnalyser.analyzeMessage();
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+		assertEquals("SAD", mood);
 	}
 
 	/**
-	 * Testing for other case
-	 * Given “I am in Any Mood” message Should Return HAPPY
+	 * Testing for other case Given “I am in Any Mood” message Should Return HAPPY
 	 */
 	@Test
-	public void message_WhenNotSad_MustReturn_Happy() {
-		analyser = new MoodAnalyser("I am a Any Mood");
-		Assert.assertEquals("HAPPY", analyser.analyzeMessage());
+	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
+		String mood = null;
+		try {
+			mood = moodAnalyser.analyzeMessage();
+		} catch (MoodAnalyserException e) {
+
+			e.printStackTrace();
+		}
+		assertEquals("HAPPY", mood);
+	}
+	/**
+	 * Given Empty Mood should Throw Mood Analysis Exception indicating EmptyMood
+	 */
+	@Test
+	public void givenMessage_WhenNull_ShouldThrowMoodAnalysisException_OfTypeNull() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+		try {
+			moodAnalyser.analyzeMessage();
+		}
+		catch(MoodAnalyserException e) {
+			assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
+		}
+	}
+
+	/**
+	 * Given Null Mood should throw Mood Analysis Exception indicating Null Mood
+	 */
+	@Test
+	public void givenMessage_WhenEmpty_ShouldThrowMoodAnalysisException_OfTypeEmpty() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("");
+		try {
+			moodAnalyser.analyzeMessage();
+		}
+		catch(MoodAnalyserException e) {
+			assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
+		}
 	}
 }
